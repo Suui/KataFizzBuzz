8164ea4f4c364b1d29bd70964e3fd0b832371684
Suui <suui.gd@gmail.com>
2016-03-19 01:26:55 +0100

    Green: say_fizz_when_the_number_is_divisible_by_three

--- KataFizzBuzzTest/FizzBuzzShould.cs
+++ KataFizzBuzzTest/FizzBuzzShould.cs
@@ -12,12 +12,19 @@ namespace KataFizzBuzzTest
 		{
 			FizzBuzz.Say(1).Should().Be("1");
 		}
+
+		[Test]
+		public void say_fizz_when_the_number_is_divisible_by_three()
+		{
+			FizzBuzz.Say(3).Should().Be("Fizz");
+		}
 	}
 
 	internal class FizzBuzz
 	{
 		public static string Say(int number)
 		{
+			if (number % 3 == 0) return "Fizz";
 			return number.ToString();
 		}
 	}
