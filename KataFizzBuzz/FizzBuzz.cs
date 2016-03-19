90a54627457384fe77c55a62d08b746c93a115a8
Suui <suui.gd@gmail.com>
2016-03-19 01:32:36 +0100

    Moved FizzBuzz logic to an extension method

--- KataFizzBuzz/FizzBuzz.cs
+++ KataFizzBuzz/FizzBuzz.cs
@@ -2,15 +2,6 @@
 {
 	public class FizzBuzz
 	{
-		public static string Say(int number)
-		{
-			if (IsDivisibleByThree(number)) return "Fizz";
-			return number.ToString();
-		}
-
-		private static bool IsDivisibleByThree(int number)
-		{
-			return number % 3 == 0;
-		}
+		public static string Say(int number) => number.FizzBuzzed();
 	}
 }
---
+++ KataFizzBuzz/FizzBuzzExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace KataFizzBuzz
+{
+	public static class FizzBuzzExtension
+	{
+		public static string FizzBuzzed(this int number)
+		{
+			if (number % 3 == 0) return "Fizz";
+			return number.ToString();
+		}
+	}
+}
