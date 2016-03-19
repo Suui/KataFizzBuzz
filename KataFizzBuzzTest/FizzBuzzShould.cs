c059d08ae3f4539324bc36e0309414b606c82fe0
Suui <suui.gd@gmail.com>
2016-03-19 01:25:42 +0100

    Green: say_the_number_when_it_is_not_divisible_by_three

---
+++ KataFizzBuzzTest/FizzBuzzShould.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace KataFizzBuzzTest
+{	
+	[TestFixture]
+	class FizzBuzzShould
+	{	
+		[Test]
+		public void say_the_number_when_it_is_not_divisible_by_three()
+		{
+			FizzBuzz.Say(1).Should().Be("1");
+		}
+	}
+
+	internal class FizzBuzz
+	{
+		public static string Say(int number)
+		{
+			return number.ToString();
+		}
+	}
+}
